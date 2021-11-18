70ffc9c8dbce66edef47caefa02cf42fb8dc23cd
slavik-os <thespacebirds@gmail.com>
2021-11-18 21:36:05 +0100

    lack tabltes responsivity

---
+++ js.js
@@ -0,0 +1,11 @@
+function closeNav() {
+    document.getElementById("side-nav-menu").style.display="none";
+    document.getElementById("side-nav-menu").style.width="100%";
+}
+
+function openNav() {
+    document.getElementById("side-nav-menu").style.display="flex";
+  
+}
+
+
