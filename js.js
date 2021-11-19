4631eff3b03614d48fbf6ab933f539b7353b9e0c
slavik-os <thespacebirds@gmail.com>
2021-11-19 15:59:17 +0100

    80%

--- js.js
+++ js.js
@@ -9,3 +9,20 @@ function openNav() {
 }
 
 
+document.getElementById("appoitmentBtn").addEventListener('click',()=> {
+    window.location.href="login.html";
+});
+
+function submitAppointment() {
+    nameInput = document.getElementById("nameInput");
+    date = document.getElementById("dateInput");
+    time = document.getElementById("timeInput");
+    res = "You have successfully submited your appointment as Mr : "+nameInput.value +" on "+ date.value + " / " +time.value  ;
+    if (date.value === "" | time.value === "") {
+        res = 'Please Fill all the fileds !'
+    }
+    else {
+        document.getElementById("appointmentField").innerText=res;
+    }
+    document.getElementById("appointmentField").innerText=res;
+}
